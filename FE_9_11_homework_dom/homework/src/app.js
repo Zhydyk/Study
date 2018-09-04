b1653bb29bbcb1b4f3498f28eb67fabee42c6b4c
Roman Zhydyk <romanzhydyk41@gmail.com>
2018-09-04 20:50:06 +0300

    homework_11

--- FE_9_11_homework_dom/homework/src/app.js
+++ FE_9_11_homework_dom/homework/src/app.js
@@ -1,35 +1,103 @@
 let rootNode = document.getElementById('root');
-let itemNumber = 1,
+let itemCounter = 0,
     maxItems = 10;
 
-// Your code goes here
+// h1 Element
+let h1El = document.createElement('h1');
+h1El.innerHTML = 'TODO Cat List';
+rootNode.appendChild(h1El);
 
-// DIV WITH INPUT HEADER
-let inputDivEl = document.createElement('div'),
-    inputEl = document.createElement('input');
+let mainHeaderEl = document.createElement('div');
+mainHeaderEl.setAttribute('class', 'header-input');
 
-
-inputDivEl.appendChild(inputEl);
-inputDivEl.setAttribute('class', 'header-input');
-inputEl.setAttribute('placeholder', 'Add New Action');
+rootNode.appendChild(mainHeaderEl);
+// inputElement
+let inputEl = document.createElement('input');
+inputEl.setAttribute('id', 'input');
 inputEl.setAttribute('type', 'text');
+inputEl.setAttribute('oninput', 'handleChange(event)')
+inputEl.setAttribute('placeholder', 'Add New Action');
 
-let addButtonEl = document.createElement('i');
+//button add and Icon add
+let addButtonEl = document.createElement('button'),
+    addIconEl = document.createElement('i');
 
-addButtonEl.setAttribute('class', 'material-icons addbutton');
-addButtonEl.innerHTML = 'add_box';
-inputDivEl.appendChild(addButtonEl);
-rootNode.appendChild(inputDivEl);
+addButtonEl.setAttribute('type', 'button');
+addButtonEl.setAttribute('id', 'add');
+addIconEl.setAttribute('onclick', 'addItem()');
+addButtonEl.setAttribute('disabled', 'true');
+
+addIconEl.setAttribute('class', 'material-icons addbutton');
+addIconEl.setAttribute('id', 'add');
+addIconEl.innerHTML = 'add_box';
+
+addButtonEl.appendChild(addIconEl);
+
+mainHeaderEl.appendChild(inputEl);
+mainHeaderEl.appendChild(addButtonEl);
 
 // hr
 let hrEl = document.createElement('hr');
 rootNode.appendChild(hrEl);
+let divListEl = document.createElement('div');
+divListEl.setAttribute('class', 'list');
+rootNode.appendChild(divListEl);
 
-// add div action under the hr
-let divActionEl = document.createElement('div');
+// ulElement
+let ulEl = document.createElement('ul');
+ulEl.setAttribute('id', 'list');
 
-divActionEl.setAttribute('class', 'list-action');
-rootNode.appendChild(divActionEl);
+divListEl.appendChild(ulEl);
+
+function handleChange(event) {
+    addButtonEl = document.getElementById('add');
+    addButtonEl.disabled = !event.target.value;
+}
+
+function addItem() {
+    let inputValue = document.getElementById('input').value;
+    if (itemCounter < maxItems && inputValue !== '') {
+        let liEl = document.createElement('li');
+        liEl.setAttribute('class', 'item-style');
+        liEl.setAttribute('draggable', 'true');
+        ulEl.appendChild(liEl);
+
+        let checkboxIcon = document.createElement('i');
+        checkboxIcon.className = 'material-icons';
+        checkboxIcon.appendChild(document.createTextNode('check_box_outline_blank'));
+        liEl.appendChild(checkboxIcon);
+        let spanEl = document.createElement('span');
+
+        spanEl.appendChild(document.createTextNode(' ' + inputValue));
+        liEl.appendChild(spanEl);
+
+        let iconEl = document.createElement('i');
+        iconEl.className = 'material-icons';
+        iconEl.appendChild(document.createTextNode('delete'));
+
+        liEl.appendChild(iconEl);
+        itemCounter++;
+        document.getElementById('input').value = '';
+        addButtonEl.disabled = 'false';
+        checkboxIcon.onclick = function() {
+            checkboxIcon.innerText = 'check_box';
+
+        }
+
+        iconEl.onclick = function() {
+            itemCounter--;
+            liEl.remove();
+            document.getElementById('warning-message').display = 'none';
+        }
+
+    } else if (inputValue === '') {
+        addButtonEl.disabled = 'false';
+    } else {
+        document.getElementById('warning-message').style.display = 'block';
+        inputEl.disabled = 'false';
+    }
+}
+
 //div element for img
 let divImgEl = document.createElement('div'),
     imgEl = document.createElement('img');
@@ -38,73 +106,3 @@ divImgEl.setAttribute('class', 'container-img');
 divImgEl.appendChild(imgEl);
 imgEl.setAttribute('src', '../homework/assets/img/cat.png');
 rootNode.appendChild(divImgEl);
-
-let warningMessageEl = document.createElement('h4');
-warningMessageEl.setAttribute('class', 'warning-message');
-warningMessageEl.innerHtml = 'Maximum item per list are created';
-
-addButtonEl.addEventListener('click', emptyNewAction);
-
-function emptyNewAction() {
-    let text = inputEl.value;
-    if (text === '') {
-        return;
-    }
-    inputEl.value = '';
-    let containerForActionEl = document.createElement('div');
-    containerForActionEl.setAttribute('class', 'container-action');
-    containerForActionEl.setAttribute('draggable', 'true');
-
-    let checkboxEl = document.createElement('input'),
-        labelEl = document.createElement('label');
-
-    checkboxEl.setAttribute('type', 'checkbox');
-    checkboxEl.setAttribute('id', 'subscribeNews');
-
-    labelEl.setAttribute('for', 'subscribeNews');
-
-    containerForActionEl.appendChild(checkboxEl);
-    containerForActionEl.appendChild(labelEl);
-
-    let EnterText = document.createElement('span');
-    EnterText.innerHtml = text;
-
-    containerForActionEl.appendChild(EnterText);
-
-    let deleteButton = document.createElement('i'),
-        markButton = document.createElement('i');
-
-    deleteButton.setAttribute('class', 'material-icons delete');
-    deleteButton.innerHTML = 'delete';
-
-    markButton.setAttribute('class', 'material-icons mark-text');
-    markButton.innerHTML = 'check_box_outline_blank';
-
-    labelEl.appendChild(markButton);
-    labelEl.appendChild(deleteButton);
-
-    divActionEl.appendChild(containerForActionEl);
-
-    if (divActionEl.childNodes.length === maxItems) {
-        rootNode.insertBefore(warningMessageEl, inputDivEl);
-        inputEl.setAttribute('disabled', 'true');
-        return;
-    } else {
-        inputEl.focus();
-    }
-
-}
-divActionEl.addEventListener('click', function(event) {
-    if (event.target.classList.contains('mark')) {
-        event.target.innerHTML = 'check-box';
-    }
-}, false);
-divActionEl.addEventListener('click', function(event) {
-    if (event.target.classList.contains('delete')) {
-        event.target.parentNode.remove();
-        if (warningMessageEl.style.display === 'block') {
-            warningMessageEl.style.display = 'none';
-            inputEl.removeAttribute('disabled');
-        }
-    }
-}, false);
